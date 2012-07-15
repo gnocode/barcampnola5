3ec853ac6732c6da2bceb78700a649521744c0da
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-15 16:39:19 -0500

    timeline and fix to entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,15 +1,15 @@
 class Entry < ActiveRecord::Base
   acts_as_taggable
-  
+
   attr_accessible :body
   attr_accessible :date
   attr_accessible :title
   attr_accessible :start_at
   attr_accessible :end_at
 
-  define_index do
-    indexes :body
-    indexes :data
-    indexes :title
-  end
+  # define_index do
+  #   indexes :body
+  #   indexes :data
+  #   indexes :title
+  # end
 end
