1d14b73294687c5324dc162df73fca730f56bf92
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-15 12:33:55 -0500

    Addinga  generic logger incase of foreman and unicorn

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Barcampnola5
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.logger = Logger.new STDOUT
   end
 end
