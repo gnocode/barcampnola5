841004f738f0afa0b022ce77d0806890a6be88b2
Brownell Chalstrom <bchalstrom@iseatz.com>
2012-07-15 14:05:57 -0500

    acts_as_taggable on entry

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,31 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120715170501) do
+ActiveRecord::Schema.define(:version => 20120715182007) do
 
   create_table "entries", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "data"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.datetime "date"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       :limit => 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
   end
 
 end
