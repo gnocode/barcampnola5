14c57be7cc8af765250fc3ab410bb73b19620144
Your Name <charles.paul@gmail.com>
2012-07-15 12:00:18 -0500

    adding new files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barcampnola5::Application.config.secret_token = '70b169f7dc32a2c5f3767e353c794a883324dcf3399a4d5a76a654bff6eb8ba0e9537b46ae41c5c4ad6567529473b0d9a39034f1e38a4d804091543e4f36a3e5'
