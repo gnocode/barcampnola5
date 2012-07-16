00d050fe6fefee46c8be541df3c8519b4fbf131c
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-16 15:01:42 -0500

    Re-adding rescue check

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-require './config/variables'
+require "./config/variables"
 
 worker_processes ENV["UNICORN_WORKERS"].to_i
 timeout ENV["UNICORN_TIMEOUT"].to_i
@@ -9,6 +9,12 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
     Rails.logger.info('Disconnected from ActiveRecord')
   end
+
+  if defined?(Resque)
+    Resque.redis.quit
+    Rails.logger.info('Disconnected from Redis')
+  end
+
   sleep 1
 end
 
@@ -16,5 +22,10 @@ after_fork do |server, worker|
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
     Rails.logger.info('Connected to ActiveRecord')
+  end
+
+  if defined?(Resque)
+    Resque.redis = ENV['REDIS_URI'] || ENV['REDIS_TO_GO']
+    Rails.logger.info('Connected to Redis')
   end
 end
