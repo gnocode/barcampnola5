dc8b8795f9f989dfd2b6d652aced9a3b6ac89cd8
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-16 15:02:06 -0500

    Adding about and contact paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Barcampnola5::Application.routes.draw do
   resources :entries
   resources :tags
   resources :sessions, only: [:new, :create, :destroy]
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
 
   match '/auth/:provider/callback', to: "sessions#create"
   match 'signout', to: 'sessions#destroy', as: 'signout'
   match 'auth/failure', to: redirect('/')
 
-  get "about" => "pages#about"
-  get "splash" => "pages#splash"
   get "timeline" => "entries#index"
   get "soon" => "pages#soon"
   root to: 'pages#splash'
