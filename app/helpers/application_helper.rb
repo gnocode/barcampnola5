8eb4d03081208c5e6563ecba9c1188faca1b18b4
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-16 17:44:38 -0500

    Adding a helper for the view title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     vation = content_tag :span, "VATION", class: "blue"
     logo = link_to nola + vation, root_path, class: "logo"
   end
+
+  def view_title(title = nil)
+    if title
+      "NOLAvation - " + @title
+    else
+      "NOLAvation"
+    end
+  end
 end
