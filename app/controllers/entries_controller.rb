c81d3e8b2a8d11a145a8605c90564ff6d10a88d2
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-16 14:59:02 -0500

    Dropping useless code from entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,91 +1,15 @@
 class EntriesController < ApplicationController
-  # GET /entries
-  # GET /entries.json
-  def index
-    if params[:tag]
-      Entry.tagged_with params[:tag]
-    else
-      @entries = Entry.all
-    end
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json
-    end
-  end
-
-  # GET /entries/1
-  # GET /entries/1.json
-  def show
-    @entry = Entry.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @entry }
-    end
-  end
-
-  # GET /entries/new
-  # GET /entries/new.json
   def new
-    @entry = Entry.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @entry }
-    end
+    @_entry = Entry.new
+    @entry = EntryDecorator.new @_entry
   end
 
-  # GET /entries/1/edit
-  def edit
-    @entry = Entry.find(params[:id])
-  end
-
-  # POST /entries
-  # POST /entries.json
   def create
-    @entry = Entry.new(params[:entry])
-
-    @entry.tag_list = params[:tags] if params[:tags].present?
-    respond_to do |format|
-      if @entry.save
-        format.html { redirect_to @entry, notice: 'Entry was successfully created.' }
-        format.json { render json: @entry, status: :created, location: @entry }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @entry.errors, status: :unprocessable_entity }
-      end
-    end
+    @_entry = Entry.new params[:entry]
+    @entry = EntryDecorator.new @_entry
   end
 
-  # PUT /entries/1
-  # PUT /entries/1.json
-  def update
-    @entry = Entry.find(params[:id])
-    @entry.save
-
-    respond_to do |format|
-      if @entry.update_attributes(params[:entry])
-        @entry.tag_list = params[:tags] if params[:tags].present?
-        @entry.save
-        format.html { redirect_to @entry, notice: 'Entry was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @entry.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /entries/1
-  # DELETE /entries/1.json
-  def destroy
-    @entry = Entry.find(params[:id])
-    @entry.destroy
-
-    respond_to do |format|
-      format.html { redirect_to entries_url }
-      format.json { head :no_content }
-    end
+  def index
+    @entries = EntryDecorator.decorate Entry.order(:start_at)
   end
 end
