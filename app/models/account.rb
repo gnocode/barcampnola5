fdd6264dda6dfcffb1efa76b0f036afcdc470da8
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-16 17:44:31 -0500

    Rewriting omniauth create method

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,15 +7,14 @@ class Account < ActiveRecord::Base
   attr_accessible :uid
 
   def self.from_omniauth(auth)
-    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
-  end
-
-  def self.create_from_omniauth(auth)
-    create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.name = auth["info"]["name"]
-      user.email = auth["info"]["email"]
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
     end
   end
 end
