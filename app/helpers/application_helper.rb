a1cad0981bda300f2f6e893499666a697b8430f4
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-16 14:59:34 -0500

    Removing parens from logo_link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,7 @@
 module ApplicationHelper
-
   def logo_link
-    nola = content_tag(:span, "NOLA")
-    vation = content_tag(:span, "VATION", :class => 'blue')
-    logo = link_to(nola + vation, root_path, :class => "logo")
+    nola = content_tag :span, "NOLA"
+    vation = content_tag :span, "VATION", class: "blue"
+    logo = link_to nola + vation, root_path, class: "logo"
   end
-
 end
