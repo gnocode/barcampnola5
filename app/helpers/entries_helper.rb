62399fc3f1ddfd4925f218ce25c84c98eeab1c08
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-16 14:58:49 -0500

    Refactoring the entries index

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,4 +1,32 @@
 module EntriesHelper
-  include ActsAsTaggableOn::TagsHelper
 
+  def first_date
+    @entries.first.start_at
+  end
+
+  def entry_dates
+    @entries.map do |entry|
+      render partial: 'entry', format: :json, locals: { entry: entry }
+    end.join(", ")
+  end
+
+  def main_timeline_headline
+    "NOLAVATION"
+  end
+
+  def main_timeline_text
+    ""
+  end
+
+  def main_timeline_media
+    "http://www.nolavation.com"
+  end
+
+  def main_timeline_caption
+    ""
+  end
+
+  def main_timeline_credit
+    ""
+  end
 end
