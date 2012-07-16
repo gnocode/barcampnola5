207fdd7fd91f85627bed7a5df149f2fc6d13bc1f
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-16 15:38:57 -0500

    Cleaning up the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Barcampnola5::Application.routes.draw do
   resources :accounts
   resources :entries
   resources :sessions, only: [:new, :create, :destroy]
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
 
   match '/auth/:provider/callback', to: "sessions#create"
   match 'signout', to: 'sessions#destroy', as: 'signout'
   match 'auth/failure', to: redirect('/')
 
-  get "timeline" => "entries#index"
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
   get "soon" => "pages#soon"
+  get "timeline" => "entries#index"
+
   root to: 'pages#splash'
 end
