be7f02809cf182cda190b148bc81f56dfee15d9e
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-16 14:51:18 -0500

    Revamping the entire thing

---
+++ config/unicorn.rb
@@ -0,0 +1,20 @@
+require './config/variables'
+
+worker_processes ENV["UNICORN_WORKERS"].to_i
+timeout ENV["UNICORN_TIMEOUT"].to_i
+preload_app true
+
+before_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
+  sleep 1
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
+end
