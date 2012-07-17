ed7ec2a70c2a58f162e66ae009eba0aa0a59106a
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-17 03:23:53 -0500

    ADding helper methods to get real values

--- app/decorators/entry_decorator.rb
+++ app/decorators/entry_decorator.rb
@@ -1,6 +1,14 @@
 class EntryDecorator < Draper::Base
   decorates :entry
 
+  def title
+    entry.title
+  end
+
+  def body
+    Rails.cache.read("entry[#{entry.id}][body]") || entry.body
+  end
+
   def creator
     if account.present?
       account.name
