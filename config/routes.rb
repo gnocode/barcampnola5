def3f7d32a8228563e9fe5b8730e8ff31d602f32
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-18 03:08:25 -0500

    Fixing oath problems

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Barcampnola5::Application.routes.draw do
     match 'search', on: :collection
   end
   resources :sessions, only: [:new, :create, :destroy]
-  match 'signout', to: 'sessions#destroy', as: :signout
+  match '/signout', to: 'sessions#destroy', as: :signout
 
-  match 'auth/:provider/callback', to: 'sessions#create'
-  match 'auth/failure', to: redirect('/')
+  match '/auth/:provider/callback', to: 'sessions#create'
+  match '/auth/failure', to: redirect('/')
 
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
-  get "soon" => "pages#soon"
-  get "timeline" => "entries#index"
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
+  get '/soon' => 'pages#soon'
+  get '/timeline' => 'entries#index'
 
   root to: 'pages#splash'
 end
